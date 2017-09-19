66fda66a2011fa920ba8cb02722bc5e6dbb8f392
bodiddlie <bodiddlie@gmail.com>
2017-09-19 16:38:20 -0700

    basic ideas, nothing really here yet

---
+++ README.md
@@ -0,0 +1,11 @@
+# Relisted
+---
+Just spitballing ideas in here for now. This will all get replaced with a proper README eventually. :P
+
+## State
+---
+State this should track internally.
+- Current sort by
+- sort direction
+- highlighted index
+- filter input value
--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import PropTypes from 'prop-types'
 
 import Relisted from '../src'
 
-storiesOf('Relisted', module).add('just columns', () => (
-  <BasicList columns={columns} />
-))
+storiesOf('Relisted', module)
+  .add('empty data', () => <BasicList columns={columns} data={[]} />)
+  .add('with data', () => <BasicList columns={columns} data={data} />)
 
 const columns = [
   { name: 'id', text: 'ID' },
@@ -14,7 +15,14 @@ const columns = [
   { name: 'bar', text: 'Bar' },
 ]
 
-function BasicList({ columns, data = [] }) {
+const data = [
+  { id: 1, title: 'Item #1', foo: 'Foo', bar: 'Bar' },
+  { id: 2, title: 'Item #2', foo: 'Foo', bar: 'Bar' },
+  { id: 3, title: 'Item #3', foo: 'Foo', bar: 'Bar' },
+  { id: 4, title: 'Item #4', foo: 'Foo', bar: 'Bar' },
+]
+
+function BasicList({ columns, data }) {
   return (
     <Relisted>
       {({ getColumnProps }) => (
@@ -26,10 +34,30 @@ function BasicList({ columns, data = [] }) {
               </div>
             ))}
           </div>
+          {data.length > 0 ? (
+            <div style={bodyStyle}>
+              {data.map(row => (
+                <div style={rowStyle} key={row.id}>
+                  {columns.map(c => (
+                    <div key={c.name} style={cellStyle}>
+                      {row[c.name]}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div style={centeredStyle}>Empty Data</div>
+          )}
         </div>
       )}
     </Relisted>
   )
+}
+
+BasicList.propTypes = {
+  columns: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
 }
 
 const containerStyle = {
@@ -43,8 +71,46 @@ const containerStyle = {
 const headerStyle = {
   display: 'flex',
   width: '100%',
+  fontSize: '0.75rem',
+  fontWeight: 'bold',
+  borderBottom: '1px solid #cdcdcd',
 }
 
 const columnStyle = {
   flex: 1,
+  paddingTop: '0.75rem',
+  paddingBottom: '0.75rem',
+  paddingLeft: '0.25rem',
+  cursor: 'pointer',
+}
+
+const bodyStyle = {
+  width: '100%',
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+}
+
+const centeredStyle = {
+  display: 'flex',
+  width: '100%',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const rowStyle = {
+  width: '100%',
+  display: 'flex',
+  backgroundColor: 'transparent',
+  fontSize: '0.75rem',
+  flexShrink: '0',
+  cursor: 'pointer',
+}
+
+const cellStyle = {
+  flex: 1,
+  paddingTop: '0.25rem',
+  paddingBottom: '0.25rem',
+  paddingLeft: '0.25rem',
 }
