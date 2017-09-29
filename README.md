dcb14eb44dab5e23c7826fb86cf908243f04aae2
bodiddlie <bodiddlie@gmail.com>
2017-09-29 15:00:31 -0700

    ready for publishing first version

--- README.md
+++ README.md
@@ -1,11 +1,176 @@
 # Relisted
----
-Just spitballing ideas in here for now. This will all get replaced with a proper README eventually. :P
 
-## State
----
-State this should track internally.
-- Current sort by
-- sort direction
-- highlighted index
-- filter input value
+React Primitives to build a sortable, filterable, data-list.
+
+## What this is
+
+Relisted is a component that manages user interaction for sorting and filtering
+a list of data, while allowing you to retain complete control over the rendering
+of the list. 
+
+## What this is NOT
+
+This is a super early version that is just barely generalized enough for me to open-source.
+It does not currently have any accessibility considerations. It does not support paging
+or virtualization. It has built-in sort and filter functionality that might not meet your needs. My next plan on the roadmap for this is to add the ability to override the filter/sort
+functionality with your own custom functions.
+
+## Installation
+
+```
+npm install --save relisted
+```
+
+or
+
+```
+yarn add relisted
+```
+
+> This package also depends on `react` and `prop-types`. Make sure those are both installed!
+
+## Simple Example
+
+```jsx
+import Relisted from 'relisted'
+
+function BasicList({columns, data}) {
+  return (
+    <Relisted>
+      {({getColumnProps}) => (
+        <div style={{display: 'flex', flexDirection: 'column', width: '500px', height: '500px', border: '1px solid blue'}}>
+          <div style={{display: 'flex', width: '100%', borderBottom: '1px solid #cdcdcd'}}>
+            {columns.map(c => (
+              <div key={c.name} {...getColumnProps((style: {flex: 1}))}>
+                {c.text}
+              </div>
+            ))}
+          </div>
+          {data.length > 0 ?(
+            <div style={{display: 'flex', width: '100%', flex: 1, flexDirection: 'column'}}>
+              {data.map(row => (
+                <div style={{width: '100%', display: 'flex' flexShrink: '0'}}>
+                  {columns.map(c => (
+                    <div key={c.name} style={{flex: 1}}>
+                      {row[c.name]}
+                    </div>
+                  ))}
+                </div>
+              ))}
+              </div>
+          ) : (
+            <div>Empty Data</div>
+          )}
+        </div>
+      )}
+    </Relisted>
+  )
+}
+
+const columns = [
+  { name: 'cost', text: 'Cost' },
+  { name: 'name', text: 'Name' },
+  { name: 'hero', text: 'Hero' },
+  { name: 'rarity', text: 'Rarity' },
+]
+
+const data = [
+  { cost: 1, name: 'Mistress of Mixtures', hero: 'Neutral', rarity: 'Common' },
+  { cost: 2, name: 'Frostbolt', hero: 'Mage', rarity: 'Basic' },
+  { cost: 3, name: 'Jade Blossom', hero: 'Druid', rarity: 'Common' },
+  { cost: 6, name: 'Dragonfire Potion', hero: 'Priest', rarity: 'Epic' },
+  { cost: 4, name: 'Mortal Strike', hero: 'Warrior', rarity: 'Rare' },
+  {
+    cost: 8,
+    name: 'Ragnaros, Lightlord',
+    hero: 'Paladin',
+    rarity: 'Legendary',
+  },
+]
+
+function App() {
+  return (
+    <BasicList
+      columns={columns}
+      data={data}
+    />
+  )
+}
+```
+
+## Props
+
+### sortBy
+
+> `string` | defaults to empty string
+
+Pass a the name of the column you want to sort by. Used by the default filter function.
+
+### children
+
+> `function({})` | *required*
+
+This function is called by the render of `relisted` and is passed an object. Read more below.
+
+## Child Callback Function
+
+The child callback function is passed an object that has 3 different categories of properties:
+
+### prop getters
+
+These are functions that will return an object representing props to apply to a component.
+Any props that you would like to apply to that component along with the ones returned
+by the getter should be passed to the getter function.
+
+#### `getColumnProps`
+
+This function is responsible for wiring up the click handling of each column header for
+sorting.
+
+Required properties:
+
+- `name`: the name of the column. This name gets set as the sortBy once a column header is clicked.
+
+#### `getFilterProps`
+
+This function applies props for controlling the state of the search filter.
+
+There are no required properties for this function.
+
+#### `getClearProps`
+
+This function applies props for wiring up a button for clearing the search filter.
+
+There are no requrired properties for this function.
+
+### helpers
+
+These are functions that provide built-in sorting and filtering of a list.
+
+| property     | type                              | description                                   |
+|--------------|-----------------------------------|-----------------------------------------------|
+| `filterData` | `function(data: [], columns: [])` | filters the given data using the filter state |
+| `sortData`   | `function(data: [])`              | sorts the given data using the sort state     |
+
+### state
+
+These are values that represent the current internal state of the Relisted component.
+
+| property        | type      | description                               |
+|-----------------|-----------|-------------------------------------------|
+| `sortBy`        | `string`  | the column name to sort by                |
+| `sortAscending` | `boolean` | whether to sort in ascending order or not |
+
+## Examples
+
+Examples can be found in the stories folder.
+
+## Inspiration
+
+This library was inspired by the awesome [Downshift](https://github.com/paypal/downshift) created
+by Kent C. Dodds. I highly recommend checking out the documentation for that library to get a better
+understanding of the patterns utilized here.
+
+## LICENSE
+
+MIT
---
+++ dist/index.js
@@ -0,0 +1,156 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+var PropTypes = _interopDefault(require('prop-types'));
+var orderBy = _interopDefault(require('lodash/orderBy'));
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var callAll = function callAll() {
+  for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
+    fns[_key] = arguments[_key];
+  }
+
+  return function () {
+    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      args[_key2] = arguments[_key2];
+    }
+
+    return fns.forEach(function (fn) {
+      return fn && fn.apply(undefined, args);
+    });
+  };
+};
+
+var Relisted = function (_React$Component) {
+  _inherits(Relisted, _React$Component);
+
+  function Relisted(props) {
+    _classCallCheck(this, Relisted);
+
+    var _this = _possibleConstructorReturn(this, (Relisted.__proto__ || Object.getPrototypeOf(Relisted)).call(this, props));
+
+    _initialiseProps.call(_this);
+
+    _this.state = {
+      filterValue: '',
+      sortBy: props.sortBy,
+      sortAscending: true
+    };
+    return _this;
+  }
+
+  _createClass(Relisted, [{
+    key: 'propGetters',
+    value: function propGetters() {
+      return {
+        getColumnProps: this.getColumnProps,
+        getFilterProps: this.getFilterProps,
+        getClearProps: this.getClearProps,
+        filterData: this.filterData,
+        sortData: this.sortData,
+        sortBy: this.state.sortBy,
+        sortAscending: this.state.sortAscending
+      };
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return this.props.children(this.propGetters());
+    }
+  }]);
+
+  return Relisted;
+}(React.Component);
+
+Relisted.propTypes = {
+  children: PropTypes.func.isRequired,
+  sortBy: PropTypes.string
+};
+Relisted.defaultProps = {
+  sortBy: ''
+};
+
+var _initialiseProps = function _initialiseProps() {
+  var _this2 = this;
+
+  this.filterData = function (data, columns) {
+    return data.filter(function (row) {
+      var keys = columns.map(function (c) {
+        return c.name;
+      });
+      return keys.some(function (key) {
+        var check = row[key];
+        if (typeof check === 'string') return check.toLowerCase().includes(_this2.state.filterValue.toLowerCase());
+        return check === _this2.state.filterValue;
+      });
+    });
+  };
+
+  this.sortData = function (data) {
+    return orderBy(data, [_this2.state.sortBy], [_this2.state.sortAscending ? 'asc' : 'desc']);
+  };
+
+  this.columnClick = function (columnName) {
+    _this2.setState(function (prevState) {
+      var state = { sortBy: columnName };
+      if (prevState.sortBy === state.sortBy) {
+        state.sortAscending = !prevState.sortAscending;
+      } else {
+        state.sortAscending = true;
+      }
+      return state;
+    });
+  };
+
+  this.getColumnProps = function () {
+    var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    var name = _ref.name,
+        rest = _objectWithoutProperties(_ref, ['name']);
+
+    return _extends({}, rest, {
+      onClick: callAll(rest.onClick, function () {
+        return _this2.columnClick(name);
+      })
+    });
+  };
+
+  this.getFilterProps = function () {
+    var props = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    return _extends({
+      value: _this2.state.filterValue
+    }, props, {
+      onChange: callAll(props.onChange, _this2.filterChange)
+    });
+  };
+
+  this.getClearProps = function () {
+    var props = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    return _extends({}, props, {
+      onClick: callAll(props.onClick, _this2.clearFilter)
+    });
+  };
+
+  this.filterChange = function (evt) {
+    _this2.setState({ filterValue: evt.target.value });
+  };
+
+  this.clearFilter = function () {
+    _this2.setState({ filterValue: '' });
+  };
+};
+
+module.exports = Relisted;
